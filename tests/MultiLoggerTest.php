d238cb3b960572e234a7e80557e4d2b88f51af21
Graham Campbell <graham@bugsnag.com>
2016-07-06 16:12:19 -0700

    Added some tests

---
+++ tests/MultiLoggerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Bugsnag\PsrLogger\Tests;
+
+use Bugsnag\PsrLogger\MultiLogger;
+use GrahamCampbell\TestBenchCore\MockeryTrait;
+use PHPUnit_Framework_TestCase as TestCase;
+use Mockery;
+
+class MultiLoggerTest extends TestCase
+{
+    use MockeryTrait;
+
+    public function testCanLog()
+    {
+        $one = Mockery::mock(LoggerInterface::class);
+        $one->shouldReceive('log')->once()->with('info', 'hi', ['foo' => 'bar']);
+        $two = Mockery::mock(LoggerInterface::class);
+        $two->shouldReceive('log')->once()->with('info', 'hi', ['foo' => 'bar']);
+
+        $multi = new MultiLogger([$one, $two]);
+
+        $multi->log('info', 'hi', ['foo' => 'bar']);
+    }
+
+    public function testWarning()
+    {
+        $one = Mockery::mock(LoggerInterface::class);
+        $one->shouldReceive('log')->once()->with('warning', 'hi!', ['foo' => 'baz']);
+        $two = Mockery::mock(LoggerInterface::class);
+        $two->shouldReceive('log')->once()->with('warning', 'hi!', ['foo' => 'baz']);
+
+        $multi = new MultiLogger([$one, $two]);
+
+        $multi->warning('hi!', ['foo' => 'baz']);
+    }
+}
